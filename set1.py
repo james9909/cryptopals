5b9f4f5a78780d308e8579d092d1573972368cc1
James Wang <jameswang9909@hotmail.com>
2017-08-02 18:48:25 -0400

    Add challenges 5-6 from set 1

--- set1.py
+++ set1.py
@@ -1,5 +1,7 @@
 import base64
-import string
+import binascii
+
+from itertools import cycle, izip
 
 # Problem 1
 def convert_hex_to_base64(s):
@@ -77,3 +79,63 @@ def detect_single_byte_xor(filename):
     return dec, k
 
 assert detect_single_byte_xor("files/4.txt") == ("Now that the party is jumping\n", 64)
+
+# ==============================================================================
+# Problem 5
+
+def repeating_key_xor(s, key):
+    """Encodes a string using repeated-key xor"""
+    enc = "".join([chr(ord(x) ^ ord(c)) for x, c in zip(s, cycle(key))])
+    return enc.encode("hex")
+
+assert repeating_key_xor("Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal", "ICE") == "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
+
+# ==============================================================================
+# Problem 6 (INCOMPLETE)
+
+def hamming_distance(s1, s2):
+    """Calculates the hamming distance between two strings"""
+    bin1 = int(binascii.hexlify(s1), 16)
+    bin2 = int(binascii.hexlify(s2), 16)
+    distance = 0
+    for x in range(len(s1) * 8):
+        distance += (bin1 & 1) ^ (bin2 & 1)
+        bin1 >>= 1
+        bin2 >>= 1
+    return distance
+
+def group_string(s, size):
+    """Groups a string into evenly sized chunks"""
+    groups = zip(*(iter(s),) * size)
+    return ["".join(g) for g in groups]
+
+def break_repeating_key_xor(s):
+    """Breaks a string encoded with a repeating-key xor"""
+
+    # Calculate probably key length based on hamming distance
+    distances = []
+    for key_length in range(2, 41):
+        groups = group_string(s, key_length)
+        distances.append((key_length, hamming_distance(groups[0], groups[1]) / key_length,))
+
+    distances = sorted(distances, key=lambda x:x[1])
+
+    max_score = 0
+    decrypted = ""
+    for candidate in distances[:min(len(distances), 5)]:
+        key = ""
+        key_length = candidate[0]
+        groups = group_string(s, key_length)
+        for x in range(key_length):
+            block = "".join([g[x] for g in groups])
+            dec, k = decode_single_byte_xor(block.encode("hex"))
+            key += chr(k)
+        tmp = repeating_key_xor(s.encode("hex"), key)
+        score = calculate_score(tmp)
+        if score > max_score:
+            max_score, decrypted = score, tmp
+
+    return decrypted
+
+f = open("files/6.txt").read()
+s = break_repeating_key_xor(base64.b64decode(f)).decode("hex")
